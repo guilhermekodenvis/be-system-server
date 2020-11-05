bf9aa1a513a7bddf66f3d9806becdd9d6a04af30
Guilherme Sartori <gui.sartori96@gmail.com>
2020-11-04 22:29:27 -0300

    resolvinf

--- src/shared/infra/typeorm/migrations/1602893928674-CreateUsersTable.ts
+++ src/shared/infra/typeorm/migrations/1602893928674-CreateUsersTable.ts
@@ -4,7 +4,6 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 export default class CreateUsersTable1602893928674
 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query('ALTER USER besystem WITH SUPERUSER')
 		await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
 		await queryRunner.createTable(
 			new Table({
