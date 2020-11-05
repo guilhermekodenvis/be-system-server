a24a76d1e8a2a286f4ceb564ba74ec43d44c91d1
Guilherme Sartori <gui.sartori96@gmail.com>
2020-11-04 21:37:46 -0300

    resolving uuid ossp

--- src/shared/infra/typeorm/migrations/1602893928674-CreateUsersTable.ts
+++ src/shared/infra/typeorm/migrations/1602893928674-CreateUsersTable.ts
@@ -4,6 +4,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 export default class CreateUsersTable1602893928674
 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
 		await queryRunner.createTable(
 			new Table({
 				name: 'users',
